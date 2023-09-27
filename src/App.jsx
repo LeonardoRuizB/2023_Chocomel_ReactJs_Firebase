ecc6daa1b53b8eaef78f207dfbba0dab99dd2e19
Leonardo Ruiz Bottura <92493176+LeonardoRuizB@users.noreply.github.com>
2023-09-26 21:04:12 -0300

    feat: adding routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
-import Header from './Components/Header/Header';
+import AppRouter from './AppRouter';
 
 function App() {
 
   return (
     <>
-      <Header/>
+      <AppRouter />
     </>
   )
 }
 
-export default App
+export default App;
---
+++ src/AppRouter.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Home from './Components/Home/Home';
+import Login from './Components/User/Login';
+
+function AppRouter() {
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/login' element={<Login />}></Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default AppRouter;
